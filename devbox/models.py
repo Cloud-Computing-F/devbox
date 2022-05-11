92324e4d7b3f88e406f8ddebf35f6a62acf76df6
neChak <34412522+cjy8922@users.noreply.github.com>
2022-05-11 09:05:26 +0900

    Add folder model

--- devbox/models.py
+++ devbox/models.py
@@ -3,10 +3,25 @@ from django.db import models
 from django.utils.timezone import now
 
 
+class Folder(models.Model):
+    folderName = models.CharField(max_length=100, default=now().strftime("%Y%m%d%H%M%S"))
+    dateTimeOfUpload = models.DateTimeField(auto_now=True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name="child_folder", null=True, blank=True)
+
+    def get_foldername(self):
+        return self.folderName
+
+
 class Files(models.Model):
     fileName = models.CharField(max_length=100, default=now().strftime("%Y%m%d%H%M%S"))
+    fileSize = models.IntegerField(default=0)
     uploadedFile = models.FileField(upload_to="UploadedFiles/")
     dateTimeOfUpload = models.DateTimeField(auto_now=True)
+    parent = models.ForeignKey(Folder, on_delete=models.CASCADE, related_name="child_files", null=True, blank=True)
 
     def get_filename(self):
-        return os.path.basename(self.uploadedFile.name)
+        return os.path.basename(self.uploadedFile.url)
+
+    def delete(self, *args, **kwargs):
+        self.uploadedFile.delete()
+        super().delete(*args, **kwargs)
