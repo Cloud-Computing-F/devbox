f597e54247557e582596cae418eb99b1900743ab
SteveArseneLee <lclgood@khu.ac.kr>
2022-05-17 12:04:52 +0900

    아직 진행중

---
+++ cloud_proj/clouduser/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Clouduser(models.Model):
+    username = models.CharField(max_length=64, verbose_name='사용자명')
+    useremail = models.EmailField(max_length=128, verbose_name='사용자이메일')
+    password = models.CharField(max_length=64, verbose_name='비밀번호')
+    registed_dttm = models.DateTimeField(auto_now_add=True, verbose_name='등록시간')
+    
+    
+    def __str__(self):
+        return self.username
+    
+    class Meta:
+        db_table = 'cloud_user'
+        verbose_name = '클라우드 사용자명'
+        verbose_name_plural = '전체 클라우드 사용자명'
