602a2d754465d766b0d9b6a5bc91271633afa2bb
ayleeee <janhyang0309@gmail.com>
2022-05-22 23:13:59 +0900

    Update mail-service feature :  main.html

--- devbox/views.py
+++ devbox/views.py
@@ -263,7 +263,7 @@ def display_file_and_folder_ui(request,uuid):
     folders = models.Folder.objects.filter(uuid=uuid)
     files = models.Files.objects.filter(parent_id=0)
     return render(request,"devbox/main.html",context={
-        "current_folder_id": 1 ,
+        "current_folder_id": 0 ,
         "files": files,
         "folders": folders,
     })
@@ -282,7 +282,7 @@ def mailing(request):
         html_message=render_to_string('devbox/mail.html',context={
             # 주소는 공유 파일/폴더 주소에 맞게 수정하면 됩니다.
             # 우선은 가장 기본적인 것으로 설정해뒀습니다.
-            'link':'127.0.0.1:8000/media/UploadedFiles/'+str(selected_target),
+            'link':selected_target,
         })
         # send_mail (메일 제목, 메일 내용, email_from,recipient_list,html_message)
         send_mail("DevBox 공유 메일","테스트 내용입니다.",settings.EMAIL_HOST_USER,recipient_list,html_message=html_message)
