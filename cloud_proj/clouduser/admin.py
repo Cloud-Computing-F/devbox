0ec22b478e9e6788510cca61eca839cb433e9997
SteveArseneLee <lclgood@naver.com>
2022-05-22 19:18:48 +0900

    계정 설정

--- cloud_proj/clouduser/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Clouduser
-# Register your models here.
-class ClouduserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'password')
-
-
-admin.site.register(Clouduser, ClouduserAdmin)
