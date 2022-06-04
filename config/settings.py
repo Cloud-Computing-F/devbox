fcca6651273fcb853f12987e1907f0fefb113d5f
ayleeee <86951396+ayleeee@users.noreply.github.com>
2022-06-04 22:40:27 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -125,7 +125,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'devbox',
-        'USER': 'devobx',
+        'USER': 'devbox',
         'PASSWORD': 'devboxpassword',
         'HOST': '127.0.0.1',
         'PORT': '3306',
